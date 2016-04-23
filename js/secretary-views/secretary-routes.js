676a79a110b33abde32a0fe810e4893953a1388e
remi.passmoilesel <remi.passmoilesel@gmail.com>
2016-04-23 10:21:01 +0200

    Removing # from URLS

--- js/secretary-views/secretary-routes.js
+++ js/secretary-views/secretary-routes.js
@@ -26,16 +26,15 @@ function SecretaryRoutes() {
  * Référencer toutes les routes disponibles d'un module
  * @param angularMod
  */
-SecretaryRoutes.prototype.registerRoutesIn = function (angularMod) {
+SecretaryRoutes.prototype.registerRoutesIn = function(angularMod) {
 
     // fabrique de template a partir d'un element, avec titre et contenu
-    var makeViewTemplate = function (elmt) {
-        return "<h1>" + elmt.label + "</h1>"
-            + elmt.template;
+    var makeViewTemplate = function(elmt) {
+        return "<h1>" + elmt.label + "</h1>" + elmt.template;
     };
 
     // creer un nom de controlleur dynamique
-    var controllerNameOf = function (elmt) {
+    var controllerNameOf = function(elmt) {
         return "ViewControllerOf_" + (elmt.urlSimpleAccess.replace(/[^\w\s]/gi, ''));
     }
 
@@ -54,58 +53,63 @@ SecretaryRoutes.prototype.registerRoutesIn = function (angularMod) {
 
 
     // declarer dynamiquement les routes
-    angularMod.config(['$routeProvider', function ($routeProvider) {
+    angularMod.config(['$routeProvider', '$locationProvider',
+        function($routeProvider, $locationProvider) {
 
-        // iterer les vues diponibles pour établir les routes possibles
-        for (var elmtK in vm.views) {
-            var elmt = vm.views[elmtK];
+            // enlever les # des URLS
+            $locationProvider.html5Mode(true);
 
-            // verifier les patterns de routes
-            if (elmt.urlPatterns instanceof Array !== true) {
-                throw constants.INVALID_ARGUMENT;
-            }
+            // iterer les vues diponibles pour établir les routes possibles
+            for (var elmtK in vm.views) {
+                var elmt = vm.views[elmtK];
 
-            // iterer les differentes routes possibe pour les déclarer
-            for (var r in elmt.urlPatterns) {
-
-                var route = elmt.urlPatterns[r];
-
-                // parametres de la route
-                var params = {};
-                params.template = makeViewTemplate(elmt);
-
-                if (typeof elmt.controller !== "undefined") {
-                    params.controllerAs = "$ctrl";
-                    params.controller = controllerNameOf(elmt);
+                // verifier les patterns de routes
+                if (elmt.urlPatterns instanceof Array !== true) {
+                    throw constants.INVALID_ARGUMENT;
                 }
 
-                // creation de la route
-                $routeProvider.when(route, params);
+                // iterer les differentes routes possibe pour les déclarer
+                for (var r in elmt.urlPatterns) {
+
+                    var route = elmt.urlPatterns[r];
+
+                    // parametres de la route
+                    var params = {};
+                    params.template = makeViewTemplate(elmt);
+
+                    if (typeof elmt.controller !== "undefined") {
+                        params.controllerAs = "$ctrl";
+                        params.controller = controllerNameOf(elmt);
+                    }
+
+                    // creation de la route
+                    $routeProvider.when(route, params);
+
+                }
 
             }
 
+            // route par défaut sur le premier element de la liste
+            $routeProvider
+                .otherwise({
+                    redirectTo: vm.views[Object.keys(vm.views)[0]].urlSimpleAccess,
+                });
         }
-
-        // route par défaut sur le premier element de la liste
-        $routeProvider
-            .otherwise({
-                redirectTo: vm.views[Object.keys(vm.views)[0]].urlSimpleAccess,
-            });
-    }]);
+    ]);
 }
 
 /**
  * Service minimaliste de mise à disposition des routes possibles
  * @constructor
  */
-var SecretaryRoutesService = function () {
+var SecretaryRoutesService = function() {
     this.views = new SecretaryRoutes().views;
-    this.getViews = function () {
+    this.getViews = function() {
         return this.views;
     }
 }
 
-module.exports = function (angularMod) {
+module.exports = function(angularMod) {
 
     // enregistrer les routes, nécéssite une première instance du service
     var rs = new SecretaryRoutes();
@@ -116,4 +120,3 @@ module.exports = function (angularMod) {
     angularMod.service(id, SecretaryRoutesService);
     return id;
 };
-
